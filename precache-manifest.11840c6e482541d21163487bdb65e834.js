3d8cf639f78885c1e64bd70b18a911c0fe148b63
Long Nguyen <nclong87@gmail.com>
2020-03-10 01:04:05 +0800

    Updates

---
+++ precache-manifest.11840c6e482541d21163487bdb65e834.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdd77aa6dde8526313080785f98077c2",
+    "url": "/ngager-pdfwriter/index.html"
+  },
+  {
+    "revision": "de99081b08af76b71d3e",
+    "url": "/ngager-pdfwriter/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "264cf2e813fd2d5a06d5",
+    "url": "/ngager-pdfwriter/static/js/2.4c376939.chunk.js"
+  },
+  {
+    "revision": "ae0727c092f1c9916f0a1bb5935705c6",
+    "url": "/ngager-pdfwriter/static/js/2.4c376939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de99081b08af76b71d3e",
+    "url": "/ngager-pdfwriter/static/js/main.c2a99b40.chunk.js"
+  },
+  {
+    "revision": "d8fcbb3aaa426b6b9bb1",
+    "url": "/ngager-pdfwriter/static/js/runtime-main.7a9cf642.js"
+  }
+]);
