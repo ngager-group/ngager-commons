7cd143dcced30ee5b68bf1131f6b07c962957223
Long Nguyen <nclong87@gmail.com>
2020-03-12 14:21:52 +0800

    Add SmartObject

--- src/smartObject.js
+++ src/smartObject.js
@@ -3,8 +3,12 @@ import _set from 'lodash/set';
 import _clone from 'lodash/clone';
 
 class SmartObject {
-  constructor(data) {
-    this.data = data;
+  constructor(data = null) {
+    if (!data) {
+      this.data = {};
+    } else {
+      this.data = _clone(data);
+    }
     this.hasChanged = false;
   }
 
@@ -32,7 +36,7 @@ class SmartObject {
   getData(hasChanged) {
     if (this.hasChanged) {
       this.hasChanged = hasChanged;
-      return _clone(this.data);
+      return this.data;
     }
     return this.data;
   }
